cd475178293e852d549c3f15b8b30e1a68c76316
sudheesh001 <sudheesh1995@outlook.com>
2015-05-23 20:36:03 +0300

    Implements twitter login and authentication and post to twitter from app

--- server.py
+++ server.py
@@ -14,6 +14,7 @@ from time import sleep
 import re, datetime
 import urllib2, urllib
 import requests, chartkick
+from flask_oauthlib.client import OAuth
  
 import logging
 from logging.handlers import SMTPHandler
@@ -49,6 +50,35 @@ def tup2float(tup):
 def get_cursor():
 	return mysql.connect().cursor()
 
+oauth = OAuth(app)
+
+from twitter import CONSUMER_KEY, CONSUMER_SECRET
+
+twitter = oauth.remote_app(
+	'twitter',
+	consumer_key= CONSUMER_KEY,
+	consumer_secret= CONSUMER_SECRET,
+	base_url='https://api.twitter.com/1.1/',
+	request_token_url='https://api.twitter.com/oauth/request_token',
+	access_token_url='https://api.twitter.com/oauth/access_token',
+	authorize_url='https://api.twitter.com/oauth/authorize',
+)
+
+
+@twitter.tokengetter
+def get_twitter_token():
+	if 'twitter_oauth' in session:
+		resp = session['twitter_oauth']
+		return resp['oauth_token'], resp['oauth_token_secret']
+
+
+@app.before_request
+def before_request():
+	g.user = None
+	if 'twitter_oauth' in session:
+		g.user = session['twitter_oauth']
+		print g.user
+
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.djt'), 404
@@ -81,7 +111,7 @@ def wall():
 		data = json.loads(resp.text)
 		pprint(data)
 		status = data["statuses"]
-		statuses = dict(status)
+		statuses = json.dumps(status)
 		return render_template('wall.djt',tweets=statuses, searchValue=searchValue)
 	return render_template('wall.djt')
 
@@ -145,6 +175,59 @@ def statistics():
 def close_db(self):
 	"""Closes the database again at the end of the request."""
 	get_cursor().close()
+
+@app.route('/twitter')
+def index():
+	tweets = None
+	if g.user is not None:
+		resp = twitter.request('statuses/home_timeline.json')
+		if resp.status == 200:
+			tweets = resp.data
+			pprint(tweets)
+		else:
+			flash('Unable to load tweets from Twitter.')
+	return render_template('tweets.djt', tweets=tweets)
+
+@app.route('/tweet', methods=['POST'])
+def tweet():
+	if g.user is None:
+		return redirect(url_for('login', next=request.url))
+	status = request.form['tweet']
+	if not status:
+		return redirect(url_for('index'))
+	resp = twitter.post('statuses/update.json', data={
+		'status': status
+	})
+	if resp.status == 403:
+		flash('Your tweet was too long.')
+	elif resp.status == 401:
+		flash('Authorization error with Twitter.')
+	else:
+		flash('Successfully tweeted your tweet (ID: #%s)' % resp.data['id'])
+	return redirect(url_for('index'))
+
+
+@app.route('/login')
+def login():
+	callback_url = url_for('oauthorized', next=request.args.get('next'))
+	return twitter.authorize(callback=callback_url or request.referrer or None)
+
+
+@app.route('/logout')
+def logout():
+	session.pop('twitter_oauth', None)
+	return redirect(url_for('index'))
+
+
+@app.route('/oauthorized')
+def oauthorized():
+	resp = twitter.authorized_response()
+	if resp is None:
+		flash('You denied the request to sign in.')
+	else:
+		session['twitter_oauth'] = resp
+	return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
 	app.debug = True
